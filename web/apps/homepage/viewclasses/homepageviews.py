d6e3584dbbcdf49477f4c3a6beb0fa39dba524f4
Jayna Kehres <jayna.kehres@apervita.com>
2019-04-12 16:06:30 -0500

    web stuff

--- web/apps/homepage/viewclasses/homepageviews.py
+++ web/apps/homepage/viewclasses/homepageviews.py
@@ -2,15 +2,21 @@ import logging
 
 log = logging.getLogger(__name__)
 
+import sys
+sys.path.append('..')
+
+from blazers import db
 from web.lib.viewclasses.blazerview import BlazersView
+
 
 class HomePageView(BlazersView):
 
     template_name = 'user/home/homepage.html'
 
     def do_get_context(self, **kwargs):
+        books = db.find('Book')
         return {
             'announcements': ['Welcome to the site!',
                               'New books are coming April 15th!'],
-            'new_books': ['Book1', 'Book2', 'Book3']
+            'new_books': books
         }
